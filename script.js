52a2107dddce38ef85d02039d266702f3fc112ae
Adam <adam.crespo101@outlook.com>
2022-02-07 20:56:39 -0500

    updates style.css, index.html, and script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "These are my current favorite bands and artists";
 
 document.querySelector("body").appendChild(h2);
